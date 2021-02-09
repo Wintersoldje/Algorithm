62fe6d63a8f9aecb070ee62fb4d30f30af3631fc
JewinJeong <wjdwpdnls@gmail.com>
2021-02-09 17:40:14 +0900

    Edit 20210209

--- Programmers/Kakao_Tuple/Programmers_Kakao_Tuple.py
+++ Programmers/Kakao_Tuple/Programmers_Kakao_Tuple.py
@@ -3,6 +3,7 @@ def solution(s):
     tmp = 0
     str = []
     t = []
+    # 숫자 구분 ( "{" , "," , "}" 제외 시키기)
     for i in range(1, len(s)-1):
         if s[i] == "{":
             tmp = i+1
@@ -15,8 +16,11 @@ def solution(s):
             tmp += 1
         else:
             t.append(s[i])
+    # 길이로 구분
     str.sort(key=lambda x:len(x))
     len_num = len(str)
+
+    # 구분된 부분에서 순서대로 위치 맞춰주기
     for i in range(len_num-1):
         for j in range(i+1):
             index = str[i + 1].index(str[j][j])
@@ -26,6 +30,7 @@ def solution(s):
                 temp = str[i + 1][j]
                 str[i + 1][j] = str[i + 1][index]
                 str[i + 1][index] = temp
+    # 가장 앞 부분만 따서 오기 
     for i in range(len_num):
         answer.append(int(str[i][i]))
 
